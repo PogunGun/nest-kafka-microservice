690f899e17f4a3c014438beea4a44a879cf5dd54
PogunGun <pavel.pogulailo@gmail.com>
2023-08-03 12:49:22 +0300

    initialize

--- apps/auth/jest.config.ts
+++ apps/auth/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nest-kafka-nx-microservice',
+  displayName: 'auth',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nest-kafka-nx-microservice',
+  coverageDirectory: '../../coverage/apps/auth',
 };
