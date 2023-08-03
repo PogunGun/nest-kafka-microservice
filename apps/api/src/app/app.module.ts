f301b78942da31ef7835c7fc1e85d88ee2e0d0fc
PogunGun <pavel.pogulailo@gmail.com>
2023-08-03 13:39:09 +0300

    api from payments and auth

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { PaymentModule } from '../payment/payments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PaymentModule],
   controllers: [],
   providers: [],
 })
